a435312f0b454d13a5ab643e698cb2c8b876dc27
ouchfil44 <saad.ouchfil@gmail.com>
2024-11-20 00:04:52 +0100

    Add files via upload

---
+++ red.php
@@ -0,0 +1,24 @@
+<?php
+// Include GeoIP library (if needed, install it via Composer or use an online service like ipstack or geoPlugin)
+
+// Fetch the visitor's IP address
+$ipAddress = $_SERVER['REMOTE_ADDR'];
+
+// Use an API to get the country based on IP address
+$geoData = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ipAddress));
+
+// Get the country name
+$country = $geoData->geoplugin_countryName;
+
+// List of countries for redirection to test1.com
+$redirectCountries = ["Canada", "United Kingdom", "United States", "Australia"];
+
+// Check the visitor's country and redirect accordingly
+if (in_array($country, $redirectCountries)) {
+    header("Location: https://test1.com");
+    exit;
+} else {
+    header("Location: https://world1.com");
+    exit;
+}
+?>
